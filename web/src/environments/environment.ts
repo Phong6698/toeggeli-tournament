ac2a0aa10d952bfff1a349bf585a3f9f4e370b31
Chiramet Phong Penglerd <Phong@hotmail.ch>
2019-12-03 23:18:59 +0100

    Angular Fire :fire:

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  version: ''
+  version: '',
+  firebase: {
+    apiKey: 'AIzaSyCSadJ-9ipdidPSNe1USD3EghKdU_XFEbI',
+    authDomain: 'toeggeli-tournament.firebaseapp.com',
+    databaseURL: 'https://toeggeli-tournament.firebaseio.com',
+    projectId: 'toeggeli-tournament',
+    storageBucket: 'toeggeli-tournament.appspot.com',
+    messagingSenderId: '111645575379',
+    appId: '1:111645575379:web:967e6240fb341cd388ac26'
+  }
 };
 
 /*
