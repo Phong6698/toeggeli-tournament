4fe0c29b4a43b6ebb8358188c837281d0ae67bbc
Phong6698 <Phong@hotmail.ch>
2019-12-11 18:57:00 +0100

    updateStatistics Cloud Function: updates RoundMatches

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,14 +23,20 @@ export const updateStatistics = functions.region('europe-west1')
                 const team2Query = admin.firestore().collection('Statistics')
                     .where('teamName', '==', createdMatch.team2);
 
+                const roundsMatchQuery = admin.firestore().collectionGroup('RoundMatches')
+                    .where('team1', '==', createdMatch.team1)
+                    .where('team2', '==', createdMatch.team2);
+
                 const promises: Promise<any>[] = [
                     transaction.get(team1Query),
-                    transaction.get(team2Query)
+                    transaction.get(team2Query),
+                    transaction.get(roundsMatchQuery)
                 ];
 
                 const docSnaps = await Promise.all(promises);
                 const team1StatisticDoc = docSnaps[0].docs[0];
                 const team2StatisticDoc = docSnaps[1].docs[0];
+                const roundMatch = docSnaps[2].docs[0];
 
                 const updateTeam1 = {
                     goalDifference:
@@ -54,8 +60,16 @@ export const updateStatistics = functions.region('europe-west1')
                         team2StatisticDoc.data().wins + 1 : team2StatisticDoc.data().wins
                 };
 
+                const updateRoundMatch = {
+                    team1Score: createdMatch.team1Score,
+                    team2Score: createdMatch.team2Score,
+                    played: true,
+                    timestamp: createdMatch.timestamp
+                };
+
                 transaction.update(team1StatisticDoc.ref, updateTeam1);
                 transaction.update(team2StatisticDoc.ref, updateTeam2);
+                transaction.update(roundMatch.ref, updateRoundMatch);
 
             } catch (error) {
                 return error;
